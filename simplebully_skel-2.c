9b7bb4b284d200c5e2fc02177005fa5cd065e030
dhoule <caine2003@gmail.com>
2019-11-26 04:51:28 -0600

    Updates simplebully_skel-2.c
    #21 #16 Forget to add the usage() function.
    Sets up code to Rx LEADER_ELECTION_MSG from pred in “other nodes.”
    The leader assigns its `current_leader` from what it receives in
    `msg[0]`.

--- simplebully_skel-2.c
+++ simplebully_skel-2.c
@@ -6,6 +6,17 @@
 int MAX_ROUNDS = 1;           // number of rounds to run the algorithm
 double TX_PROB = 1.0 - ERROR_PROB;    // probability of transmitting a packet successfully
 
+static void usage() {
+  const char *params =
+  "Usage: [-c current_leader] [-m rounds_to_run] [-t TX_pass/fail_prob]\n"
+  " -c current_leader : integer value less than the number of procs\n"
+  " -m MAX_ROUNDS     : number of rounds to run the algorithm\n"
+  " -t TX_PROB        : packet trasnmission success/failure probability\n\n";
+  fprintf(stderr,"%s",params);
+  MPI_Finalize();
+  exit(-1);
+}
+
 unsigned long int get_PRNG_seed() {
   struct timeval tv;
   gettimeofday(&tv,NULL);
@@ -40,6 +51,8 @@ bool try_leader_elect() {
   
   return leader_elect;
 }
+
+double get_prob(){}
 
 
 int main(int argc, char *argv[]) {
@@ -102,7 +115,7 @@ int main(int argc, char *argv[]) {
   int succ = (myrank + 1) % np; // succ = successor on ring;
   int pred = (myrank + np - 1) % np; // pred = predecessor on ring
   int recv_size;
-  int round;
+  int round = 1;
   int msg[2];
 
   for (round = 0; round < MAX_ROUNDS; round++) {
@@ -129,43 +142,61 @@ int main(int argc, char *argv[]) {
     
       // Now FIRST issue a speculative MPI_IRecv() to receive data back
       int recv_buf[2];
-      bool flag = false;    // Will keep track of whether a message was received before time out
-      // YOUR CODE GOES HERE  
-    
+      int flag = 0;    // Will keep track of whether a message was received before time out
+      double startTime = MPI_Wtime();
+      MPI_Status lStatus;
+      MPI_Request lRequest;
+      
+      // int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Request *request)
+      MPI_Irecv(&recv_buf, recv_size, MPI_INT, pred, MPI_ANY_TAG, comm, &lRequest);
       // Next, you need to check if time out has occured. If time out, then you need to cancel the earlier issued speculative MPI_Irecv. 
-      // YOUR CODE GOES HERE 
+      while(!flag) {
+        // int MPI_Test(MPI_Request *request, int *flag, MPI_Status *status)
+        MPI_Test(&lRequest, &flag, &lStatus);
+        if((MPI_Wtime() - startTime) >= TIME_OUT_INTERVAL)
+          break;
+      }
       
       // We receive the message from predecessor node and decide appropriate action based on the message TAG
-      // YOUR CODE GOES HERE
-      if (flag) {
-        // If HELLO MSG received, do nothing
-        // If LEADER ELECTION message, then determine who is the new leader and send out a new leader notification message
+      if (flag) {    
         switch (status.MPI_TAG) {
           case HELLO_MSG_TAG:
+            // If HELLO MSG received, do nothing
             printf("\n[rank %d][%d] HELLO MESSAGE completed ring traversal!\n", myrank, round);
             fflush(stdout);
             break;
           case LEADER_ELECTION_MSG_TAG:
+            // If LEADER ELECTION message, then determine who is the new leader and send out a new leader notification message
+            current_leader = recv_buf[0];
             // Send a new leader message
+            // TODO
             printf("\n[rank %d][%d] NEW LEADER FOUND! new leader = %d, with token = %d\n", myrank, round, current_leader, recv_buf[1]);
             fflush(stdout);
             break;
           default: ;  // do nothing
         }
+      } else {
+        // If time out, then you need to cancel the earlier issued speculative MPI_Irecv.
+        // int MPI_Cancel(MPI_Request *request)
+        MPI_Cancel(&lRequest);
+        // int MPI_Request_free(MPI_Request *request)
+        MPI_Request_free(&lRequest);
       }
     } else {
       // Wait for a message to arrive until time out occurs
       int recv_buf[2];
+      MPI_Status nStatus;
+      MPI_Request nRequest;
 
-      if (is_not_timeout(MPI_Wtime(), pred, &status)) {
+      if (is_not_timeout(MPI_Wtime(), pred, &nStatus)) {
         // You want to first receive the message so as to remove it from the MPI Buffer 
         // Then determine action depending on the message Tag field
-        MPI_Get_count(&status, MPI_INT, &recv_size);
+        MPI_Get_count(&nStatus, MPI_INT, &recv_size);
         // int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Request *request)
-        MPI_Irecv(&recv_buf, recv_size, MPI_INT, pred, status.MPI_TAG, comm, &request);
-        MPI_Wait(&request, &status);
+        MPI_Irecv(&recv_buf, recv_size, MPI_INT, pred, nStatus.MPI_TAG, comm, &nRequest);
+        MPI_Wait(&nRequest, &nStatus);
         // determine type of msg Rx
-        if (status.MPI_TAG == HELLO_MSG_TAG) {
+        if (nStatus.MPI_TAG == HELLO_MSG_TAG) {
           // Forward the message to next node
           // With a probability 'p', forward the message to next node
           // This simulates link or node failure in a distributed system
@@ -177,35 +208,53 @@ int main(int argc, char *argv[]) {
             printf("\n\t[rank %d][%d] Not doing anything. This simulates link or node failure in a distributed system.\n", myrank, round);
             fflush(stdout);
           }
-          
         } else if (status.MPI_TAG == LEADER_ELECTION_MSG_TAG) {
           // Fist probabilistically see if wants to become a leader.
           // If yes, then generate own token and test if can become leader.
+          mytoken = generate_token();
           // If can become leader, then update the LEADER ELECTION Message appropriately and retransmit to next node
-          // Otherwise, just forward the original received LEADER ELECTION Message
           
-          if ( ) {
-            printf("\n\t[rank %d][%d] My new TOKEN = %d\n", myrank, round, mytoken);
-            fflush(stdout);
+          if (get_prob() > TX_PROB) {
+            if ((mytoken > recv_buf[1]) || ((mytoken == recv_buf[1]) && (myrank > recv_buf[0]))) {
+              msg[0] = myrank;
+              msg[1] = mytoken;
+              printf("\n\t[rank %d][%d] My new TOKEN = %d\n", myrank, round, mytoken);
+              fflush(stdout);
+            } else {
+              msg[0] = recv_buf[0];
+              msg[1] = recv_buf[1];
+              printf("\n\t[rank %d][%d] Will not participate in Leader Election because it was outranked.\n", myrank, round);
+              fflush(stdout);
+            }
           } else {
+            // Otherwise, just forward the original received LEADER ELECTION Message
+            msg[0] = recv_buf[0];
+            msg[1] = recv_buf[1];
             printf("\n\t[rank %d][%d] Will not participate in Leader Election.\n", myrank, round);
             fflush(stdout);
           }
 
           // Forward the LEADER ELECTION Message
-
+          MPI_Send(&msg, 2, MPI_INT, succ, LEADER_ELECTION_MSG_TAG, comm);
+          printf("\n\t[rank %d][%d] Sent LEADER_ELECTION_MSG_TAG Msg to node %d with TOKEN = %d\n", myrank, round, succ, recv_buf[1]);
+          
+          
+          // Forward the LEADER ELECTION RESULT MESSAGE
+          // int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
+          MPI_Recv(&recv_buf, recv_size, MPI_INT, pred, LEADER_ELECTION_RESULT_MSG_TAG, comm, &status);
           // Finally, wait to hear from current leader who will be the next leader
           printf("\n\t[rank %d][%d] NEW LEADER :: node %d with TOKEN = %d\n", myrank, round, current_leader, recv_buf[1]);
           fflush(stdout);
-          
-          // Forward the LEADER ELECTION RESULT MESSAGE
-        }   
+          current_leader = recv_buf[0];
+        } 
       } 
     }
     // Finally hit barrier for synchronization of all nodes before starting new round of message sending
+    MPI_Barrier(comm);
   }
   printf("\n** Leader for NODE %d = %d\n", myrank, current_leader);
 
   // Finalize MPI for clean exit
+  MPI_Finalize();
   return 0;
 }
